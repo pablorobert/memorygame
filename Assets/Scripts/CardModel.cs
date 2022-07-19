682872bb6cda38f85c9c6d612e3bf092d2bde33f
Pablo Alves <pablorobert@gmail.com>
2022-07-18 23:25:55 -0300

    First commit

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardModel : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    public Sprite[] faces;
+    public Sprite cardBack;
+    public int cardIndex; // e.g. faces[cardIndex];
+    public bool isFaceUp;
+
+    private GameController gameController;
+
+    public void ToggleFace(bool showFace)
+    {
+        if (showFace)
+        {
+            spriteRenderer.sprite = faces[cardIndex];
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+        }
+    }
+
+    public void Toggle()
+    {
+        isFaceUp = !isFaceUp;
+        ToggleFace(isFaceUp);
+    }
+
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        isFaceUp = false; //durante debug
+    }
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+        //Toggle();
+    }
+
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while
+    /// over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseDown()
+    {
+        if (isFaceUp == true || gameController.processando == true) {
+            return;
+        } else if (isFaceUp == false && gameController.lastCard == null) {
+            //é a primeira carta
+            //isFaceUp = true;
+            gameController.lastCard = this;
+            Toggle();
+        } else if (gameController.lastCard != null && gameController.lastCard.cardIndex == this.cardIndex) {
+            print ("é a mesma");
+            gameController.lastCard = null;
+            Toggle();
+            gameController.AlterarTentativas();
+        } else if (gameController.lastCard != null &&  gameController.lastCard.cardIndex != this.cardIndex) {
+            print ("diferente");
+            Toggle();
+            gameController.AlterarTentativas();
+            StartCoroutine(ToggleAgain());
+        }
+        
+    }
+
+    IEnumerator ToggleAgain() {
+        gameController.processando = true;
+        yield return new WaitForSeconds(1f);
+
+        gameController.lastCard.Toggle();
+        gameController.lastCard = null;
+        
+        Toggle();
+
+        gameController.processando = false;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour 
+{
+    public int[] cardIndexes;
+    public CardModel[] cards;
+
+    public CardModel lastCard;
+
+    public int jogadas;
+
+    public bool processando;
+
+    public Text tentativas;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        lastCard = null;
+        jogadas = 0;
+        processando = false;
+
+        cards = FindObjectsOfType<CardModel>();
+
+        if (cards.Length == 0) return;
+
+        int numberOfIndexes = cards.Length / 2;
+        cardIndexes = new int[numberOfIndexes];
+        for (int i = 0; i < numberOfIndexes; i++) {
+            cardIndexes[i] = -1;
+        }
+
+        int position = 0;
+        while (true) {
+        //for (int i = 0; i < numberOfIndexes; i++) {
+unique:
+            int newCard = Random.Range(0, 51);
+            //print ("novo " + newCard);
+            for (int j = 0; j < numberOfIndexes; j++) {
+                if (newCard == cardIndexes[j]) {
+                    print (newCard + " repetido");
+                    goto unique;
+                }
+            }
+            //print ("salva novo card");
+            cardIndexes[position] = newCard;
+            position++;
+            if (position == numberOfIndexes) break;
+        }
+        //return;
+        //associa de 2 em 2
+        for (int i = 0; i < numberOfIndexes; i++) {
+            cards[i].cardIndex = cardIndexes[i];
+            cards[i + numberOfIndexes].cardIndex = cardIndexes[i];
+        }
+       //swaps
+        int numberOfChanges = Random.Range(20, 30);
+        for (int i = 0; i < numberOfChanges; i++) {
+            int pos1 = Random.Range(0, cards.Length);
+            int pos2 = Random.Range(0, cards.Length);
+
+            int temp = cards[pos1].cardIndex;
+            cards[pos1].cardIndex = cards[pos2].cardIndex;
+            cards[pos2].cardIndex = temp;
+
+        }
+    }
+    
+    public void AlterarTentativas()
+    {
+        jogadas++;
+        tentativas.text = "Tentativas: " + jogadas;
+
+    }
+
+    /// <summary>
+    /// Reset is called when the user hits the Reset button in the Inspector's
+    /// context menu or when adding the component the first time.
+    /// </summary>
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
